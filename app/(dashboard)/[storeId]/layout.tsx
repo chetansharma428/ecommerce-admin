4ce57032f8016bc6d9ed1bcd181e53de672fb655
Chetan Sharma <106821304+chetansharma428@users.noreply.github.com>
2023-12-15 01:09:48 +0530

    dashboard created till settings

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,39 @@
+
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
+
+export default async function DashboardLayout({
+    children,
+    params
+}: {
+    children: React.ReactNode;
+    params: {storeid: string}
+}) {
+    const {userId} = auth();
+
+    if(!userId){
+        redirect('/sign-in');
+    }
+    console.log(userId);
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeid,
+            userId
+        }
+    });
+
+    if(!store){
+        redirect('/');
+    }
+
+    return(
+        <>
+        <Navbar />
+        {children}
+        </>
+    )
+  }
+  
---
+++ components/ui/heading.tsx
@@ -0,0 +1,18 @@
+interface HeadingPageProps {
+    title: string;
+    description: string;
+}
+
+export const Heading : React.FC<HeadingPageProps> = ({
+    title,
+    description
+}) => {
+    return(
+        <div>
+            <h2 className=" text-3xl font-bold tracking-tight">{title}</h2>
+            <p className="text-sm text-muted-foreground">
+                {description}
+            </p>
+        </div>
+    )
+}
