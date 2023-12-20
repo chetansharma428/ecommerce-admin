0b201eb92968a561bc098f7dde20f681c4c4fd10
Chetan Sharma <106821304+chetansharma428@users.noreply.github.com>
2023-12-20 14:00:38 +0530

    vercil speed shift added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ModalProvider />
         {children}
         </ThemeProvider>
+        <SpeedInsights />
         </body>
     </html>
     </ClerkProvider>
